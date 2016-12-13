5b4c0e8faf477d160dcb549d1b624d3dab219fae
Mike Schutte <tmikeschutte@gmail.com>
2016-12-13 07:49:24 -0700

    add failing feature test for create new artist

---
+++ spec/features/users_creates_an_artist_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "User submits a new artist" do
+  scenario "they enter a new artist's info"do
+    artist_name = "Caamp"
+    artist_image_path = "https://i.ytimg.com/vi/Glf6qmJ2dm8/maxresdefault.jpg"
+
+    visit '/artists'
+    click_on 'New Artist'
+    fill_in "artist_name", with: artist_name
+    fill_in "artist_image_path", with: artist_image_path
+    click_on "Add Artist"
+    expect(page).to have_content artist_name
+    expect(page).to have_css "img[src=\"#{artist_image_path}\"]"
+  end
+end
