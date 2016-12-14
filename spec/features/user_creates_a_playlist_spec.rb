6ec025f00895a5eed3fb44b025d6e8ad94312a14
Mike Schutte <tmikeschutte@gmail.com>
2016-12-13 21:29:10 -0700

    Add link check to song links

--- spec/features/user_creates_a_playlist_spec.rb
+++ spec/features/user_creates_a_playlist_spec.rb
@@ -1,15 +1,3 @@
-=begin
-As a user
-Given that songs and artists exist in the database
-When I visit the playlists path
-And I click "New playlist"
-And I fill in a name
-And I select the songs for the playlist
-And I click "Create Playlist"
-Then I see the playlist title
-And I see the titles of all songs in that playlist
-And the titles should link to the individual song pages
-=end
 require 'rails_helper'
 
 RSpec.feature "User creates a playlist" do
@@ -34,6 +22,8 @@ RSpec.feature "User creates a playlist" do
       expect(page).to have_link song2.title, href: song_path(song2)
       expect(page).to have_link song3.title, href: song_path(song3)
     end
+    click_on song1.title
+    expect(current_path).to eq(song_path(song1))
   end
 
 end
