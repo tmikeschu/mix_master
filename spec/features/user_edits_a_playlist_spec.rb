84902ce7ef90202837ffc57207bcaa520bf7b283
Mike Schutte <tmikeschutte@gmail.com>
2016-12-14 06:04:36 -0700

    Update edit playlist dream test

--- spec/features/user_edits_a_playlist_spec.rb
+++ spec/features/user_edits_a_playlist_spec.rb
@@ -18,19 +18,26 @@ RSpec.feature "User edits a playlist" do
     @playlist = create(:playlist_with_songs)
     create_list(:song, 10)
     @songs = Song.all
-    require 'pry'; binding.pry
   end
 
   scenario "they update the playlist" do
     new_playlist_name = "Chill Vibes"
     new_song = @songs.last
+    existing_song = @playlist.songs.first
+
     visit playlist_path(@playlist)
+    expect(page).to_not have_content new_playlist_name
+    expect(page).to have_link existing_song.title, song_path(existing_song)
+    expect(page).to_not have_link new_song.title, song_path(new_song)
     click_on "Edit"
+
     fill_in "playlist_name", with: new_playlist_name
     check("song-#{new_song.id}")
-    expect(page).to_not have_content new_playlist_name
+    uncheck("song-#{existing_song.id}")
     click_on "Update Playlist"
+
     expect(page).to have_content new_playlist_name
-    expect(page).to have_link new_song.name, song_path(new_song)
+    expect(page).to have_link new_song.title, song_path(new_song)
+    expect(page).to_not have_link existing_song.title, song_path(existing_song)
   end
 end
