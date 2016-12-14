46cf8810533caf437a51e3e1df7950fb49d7c972
Mike Schutte <tmikeschutte@gmail.com>
2016-12-14 13:10:20 -0700

    Add validations and sad path test for create song

--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -4,19 +4,37 @@ RSpec.feature "User creates a song" do
 
   before do
     @artists = create_list(:artist, 5)
+    @artist = @artists.first
+    visit artists_path
   end
 
   scenario 'they see the page for the individual song' do
-    artist = @artists.first
     song_title = "Misty"
-    visit artists_path
-    click_on artist.name
+    click_on @artist.name
     expect(page).to_not have_content song_title
     click_on "New Song"
     fill_in "song_title", with: song_title
     click_on "Create Song"
     expect(page).to have_content song_title
-    expect(page).to have_link artist.name, href: artist_path(artist)
+    expect(page).to have_link @artist.name, href: artist_path(@artist)
+  end
+=begin
+As a user
+Given that an artist exists in the database
+When I visit the individual artist page
+And I click "New song"
+And I click "Create Song"
+Then I should see "Title cannot be blank" on the page
+=end
+  
+  context "they forget a title" do
+    scenario "they get an error message" do
+      click_on @artist.name
+      click_on "New Song"
+      click_on "Create Song"
+      # save_and_open_page
+      expect(page).to have_content "Title can't be blank"
+    end
   end
 
 end
