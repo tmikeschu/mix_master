da6e8b6c757bb28efc0f73fca6c2afec9468a78e
Mike Schutte <tmikeschutte@gmail.com>
2016-12-14 13:15:30 -0700

    Add feature test for edit song

--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -18,21 +18,12 @@ RSpec.feature "User creates a song" do
     expect(page).to have_content song_title
     expect(page).to have_link @artist.name, href: artist_path(@artist)
   end
-=begin
-As a user
-Given that an artist exists in the database
-When I visit the individual artist page
-And I click "New song"
-And I click "Create Song"
-Then I should see "Title cannot be blank" on the page
-=end
-  
+
   context "they forget a title" do
     scenario "they get an error message" do
       click_on @artist.name
       click_on "New Song"
       click_on "Create Song"
-      # save_and_open_page
       expect(page).to have_content "Title can't be blank"
     end
   end
