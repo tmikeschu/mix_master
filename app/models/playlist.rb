bfb8e732c8839e693359dd37cd807cb31a4a81d1
Mike Schutte <tmikeschutte@gmail.com>
2016-12-13 21:30:25 -0700

    Refactor song_ids=

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,9 +4,8 @@ class Playlist < ApplicationRecord
   
 
   def song_ids=(song_ids)
-    songs = song_ids.map do |song_id|
+    self.songs = song_ids.map do |song_id|
       Song.find(song_id)
     end
-    self.songs = songs
   end
 end
