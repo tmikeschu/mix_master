9b2097a3b8d5d26b4ab27a41ab4bcd48e77aaa7b
Mike Schutte <tmikeschutte@gmail.com>
2016-12-14 17:33:53 -0700

    Delete user story comment from song index test

--- spec/features/user_views_all_songs_for_artist_spec.rb
+++ spec/features/user_views_all_songs_for_artist_spec.rb
@@ -1,12 +1,3 @@
-=begin
-As a user
-Given that an artist and that artist's associated songs exist in the database
-When I visit the individual artist page
-And I click "View songs"
-Then I should see the song titles for that specific artist sorted alphabetically
-And each title should link to the individual song page
-rescue => exception
-=end
 require 'rails_helper'
 
 RSpec.feature "User views all songs for an artist" do
