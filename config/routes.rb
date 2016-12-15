884123c42f731686fe30cfe798d6149315111522
Mike Schutte <tmikeschutte@gmail.com>
2016-12-15 07:58:49 -0700

    Refactor songs resource
    From only to except :patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:new, :index, :create, :show, :edit, :update, :destroy]
+    resources :songs, except: [:patch]
   end
   resources :songs, only: [:show] 
   resources :playlists, only: [:index, :new, :create, :show, :edit, :update]
